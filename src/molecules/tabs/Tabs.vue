4ba4509cf3ade5e891c228a5dfa43a0f77968b09
Ola <ola.frankowska@snow.dog>
2021-05-21 21:23:51 +0200

    update tabs component

--- src/molecules/tabs/Tabs.vue
+++ src/molecules/tabs/Tabs.vue
@@ -2,11 +2,14 @@
 
 <script>
 import ATabs from './Tabs.js'
-
-import AButton from '../../atoms/button/Button.vue'
+import AButton from '@atoms/button/Button.vue'
+import AIconExpandMore from '@atoms/icon/templates/IconExpandMore.vue'
 
 export default {
-  components: { AButton },
+  components: {
+    AButton,
+    AIconExpandMore
+  },
   mixins: [ATabs]
 }
 </script>
